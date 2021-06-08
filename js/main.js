4c9aad6e8fe321193a6ee65ebd724c1bd8da89b9
Nikita Britov <btitov.nd@gmail.com>
2021-06-08 18:47:30 +0300

    Исправление ошибок линтера

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function checkLength(string, maxLength) {
 
 
 getRandomNumberOfRange();
-// checkLength();
+checkLength('aa', 2);
 
 const commentMessages = [
   'Всё отлично!',
@@ -22,8 +22,8 @@ const commentMessages = [
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-]
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
 
 const commentNames = [
   'Артём',
@@ -35,20 +35,8 @@ const commentNames = [
   'Алёша',
   'Виктория',
   'Давид',
-  'Гарсон'
-]
-// const comments = [];
-
-// for (let i = 0; i < 3; i++) {
-//   comments[i] = {
-//     message: commentMessages[getRandomNumberOfRange(0, commentMessages.length - 1)],
-//     name: commentNames[getRandomNumberOfRange(0, commentNames.length - 1)],
-//     id: i + 1001,
-//     avatar : `img/avatar-${getRandomNumberOfRange(1, 6)}`
-//   }
-// }
-
-// console.log(comments);
+  'Гарсон',
+];
 
 function createComments(quantity = 3) {
   const comments = [];
@@ -58,34 +46,34 @@ function createComments(quantity = 3) {
       message: commentMessages[getRandomNumberOfRange(0, commentMessages.length - 1)],
       name: commentNames[getRandomNumberOfRange(0, commentNames.length - 1)],
       id: i + 1001,
-      avatar : `img/avatar-${getRandomNumberOfRange(1, 6)}`
-    }
+      avatar : `img/avatar-${getRandomNumberOfRange(1, 6)}`,
+    };
   }
 
   return comments;
 }
 
 function createPhotoDescription(currentIndex) {
-  let id = currentIndex;
-  let url =  currentIndex;
-  let description = 'Краткость - сестра таланта, поэтому комментарий такой небольшой. Фото - пушшшка!';
-  let likes = getRandomNumberOfRange(15, 200);
-  let comments = createComments(3);
+  const id = currentIndex;
+  const url =  currentIndex;
+  const description = 'Краткость - сестра таланта, поэтому комментарий такой небольшой. Фото - пушшшка!';
+  const likes = getRandomNumberOfRange(15, 200);
+  const comments = createComments(3);
 
-  let photoDescription = {
+  const photoDescription = {
     id,
     url,
     description,
     likes,
-    comments
-  }
+    comments,
+  };
 
   return photoDescription;
 }
 
-const descriptionArray = new Array(25).fill('').map((obj, index) => {
-  return createPhotoDescription(index);
-});
-console.log(descriptionArray);
+function createPhotosDescriptionArray(quantity) {
+  const descriptionArray = new Array(quantity).fill('').map((obj, index) => createPhotoDescription(index));
+  return descriptionArray;
+}
 
-
+createPhotosDescriptionArray(25);
