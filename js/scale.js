b8bbb6ef59cc119bc11ede7cddfad70b6aa5b0aa
Nikita Britov <btitov.nd@gmail.com>
2021-07-11 22:14:44 +0300

    Изменение размера

---
+++ js/scale.js
@@ -0,0 +1,44 @@
+const SCALE_CHANGE_STEP = 25;
+const MIN_SCALE_VALUE = 25;
+const MAX_SCALE_VALUE = 100;
+
+const imagePreview = document.querySelector('.img-upload__preview').querySelector('img');
+const scaleField = document.querySelector('.scale');
+const smallerButton = scaleField.querySelector('.scale__control--smaller');
+const biggerButton = scaleField.querySelector('.scale__control--bigger');
+const scaleInput = scaleField.querySelector('.scale__control--value');
+
+scaleInput.value = '100%';
+
+const increaseScale = () => {
+  let value = scaleInput.value;
+  value = Number(value.slice(0, value.length - 1)) + SCALE_CHANGE_STEP;
+  if (value > MAX_SCALE_VALUE) {
+    value = MAX_SCALE_VALUE;
+  }
+
+  return value = `${value}%`;
+};
+
+const decreaseScale = () => {
+  let value = scaleInput.value;
+  value = Number(value.slice(0, value.length - 1)) - SCALE_CHANGE_STEP;
+  if (value < MIN_SCALE_VALUE) {
+    value = MIN_SCALE_VALUE;
+  }
+
+  return value = `${value}%`;
+};
+
+const setScale = (value) => {
+  scaleInput.value = value;
+  imagePreview.style.transform = `scale(${value})`;
+};
+
+smallerButton.addEventListener('click', () => {
+  setScale(decreaseScale());
+});
+
+biggerButton.addEventListener('click', () => {
+  setScale(increaseScale());
+});
