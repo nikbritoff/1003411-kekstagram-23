09f37f6259b368bc8f2c7677f61ffa21bbd953da
Nikita Britov <btitov.nd@gmail.com>
2021-07-18 11:38:43 +0300

    Сортировка фото

--- js/main.js
+++ js/main.js
@@ -6,9 +6,14 @@ import {setPicturesListener} from './full-size-picture.js';
 import './upload.js';
 import {fetchData} from './api.js';
 
+import './sort.js';
+import { sorting } from './sort.js';
+
 const onDataLoad = (data) => {
   renderPictures(data);
   setPicturesListener(data);
+
+  sorting(data);
 };
 
 const onDataFail = () => {
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,11 @@
 const checkLength = (string, maxLength) => string.length <= maxLength;
 
-export {checkLength};
+const getRandomNumberInRange = (min, max) => {
+  if (min < 0 || min >= max) {
+    throw new Error('This function accepts values greater than zero');
+  }
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export {checkLength, getRandomNumberInRange};
