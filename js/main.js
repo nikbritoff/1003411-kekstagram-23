ddd900538ccf947e9fe80d6314cc0d45648d296d
Nikita Britov <btitov.nd@gmail.com>
2021-07-18 13:38:07 +0300

    Слияние

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ import {setPicturesListener} from './full-size-picture.js';
 import './upload.js';
 import {fetchData} from './api.js';
 import { sorting } from './sort.js';
-import { debounce } from './utils.js';
 
 const onDataLoad = (data) => {
   renderPictures(data);
@@ -16,11 +15,5 @@ const onDataLoad = (data) => {
 const onDataFail = () => {
   showErrorMessage('Ошибка загрузки данных. Повторите попытку позже.');
 };
-
-const cb = () => {
-  console.log(221);
-}
-
-debounce(cb, 1000);
 
 fetchData(onDataLoad, onDataFail);
